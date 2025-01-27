026b342ce644a4accf98e5a1b35ab10abc9cfbcd
Ksawlii <ksawery.blaszczak@proton.me>
2025-01-27 15:13:27 +0100

    mt6768-common: Use xiaomi power AIDL hal

--- mt6768.mk
+++ mt6768.mk
@@ -306,7 +306,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_PACKAGES += \
     libmtkperf_client_vendor \
@@ -413,7 +413,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/xiaomi \
     hardware/mediatek/libmtkperf_client \
-    hardware/lineage/interfaces/power-libperfmgr \
+    device/xiaomi/mt6768-common/power-libperfmgr \
     vendor/fingerprint/opensource/interfaces \
     $(COMMON_PATH)
 

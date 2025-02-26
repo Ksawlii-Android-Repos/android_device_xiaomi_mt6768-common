d2b4ef376b3e3afd980b430e87e61cfca4ee7ae5
Ksawlii <ksawery.blaszczak@proton.me>
2025-02-26 15:44:14 +0100

    mt6768-common: Don't build FMRadio
    I'm done with FMRadio, fuck off libfmjni.
    
    error: packages/apps/FMRadio/Android.bp:17:1: FMRadio depends on undefined module libfmjni.
    Module FMRadio is defined in namespace . which can read these 12 namespaces: [. . vendor/gms/common vendor/xiaomi/merlinx hardware/xiaomi vendor/xiaomi/mt6768-common device/xiaomi/mt6768-common hardware/mediatek hardware/google/pixel hardware/google/interfaces device/xiaomi/merlinx hardware/mediatek/libmtkperf_client]
    Module libfmjni can be found in these namespaces: [packages/apps/FMRadio/jni/fmr]
    Or did you mean [libgljni libjni librsjni]?
    fatal errors encountered
    12:22:20 soong bootstrap failed with: exit status 1
    ninja: build stopped: subcommand failed.
    
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,41 +1,10 @@
-LOS_VERSION="lineage-22.1"
-
 # mediatek codec patch from hipexscape
 if [ ! -f "frameworks/av/.mtk_codec_fix" ]; then
     curl -sL "https://raw.githubusercontent.com/hipexscape/Scripts/tsm/mediatek_codec_patch.sh" | bash
     touch frameworks/av/.mtk_codec_fix
 fi
 
-# Fix FMRadio
-if [ -d "packages/apps/FMRadio" ] && [ ! -f "packages/apps/FMRadio/.fixed-fmr" ]; then
+# Remove FMRadio
+if [ -d "packages/apps/FMRadio" ]; then
   rm -rf "packages/apps/FMRadio"
-  echo "Removed existing FMRadio, cloning new one..."
-fi
-
-if [ ! -f "packages/apps/FMRadio/.fixed-fmr" ]; then
-  git clone https://github.com/LineageOS/android_packages_apps_FMRadio -b $LOS_VERSION "packages/apps/FMRadio"
-  cd "packages/apps/FMRadio"
-  echo ""
-  echo "Fixing FMRadio..."
-  git revert --no-edit f3eb07a311caf15f4f86f8f4dd6a221d61757aed
-  touch ".fixed-fmr"
-  echo ".fixed-fmr" > .gitignore
-  echo ""
-  echo "Fixed FMRadio"
-  cd ../../../
-fi
-
-# if libfmjni is undefined
-if [ -f "out/error.log" ]; then
-  if grep -r -w "libfmjni" out/error.log; then
-    cd vendor/xiaomi/mt6768-common
-    if [ -f .git/shallow ]; then
-      git pull --unshallow
-      git revert --no-edit 0f711e758c005a41fcc396ba14ad89b557fb402b
-    else
-      git revert --no-edit 0f711e758c005a41fcc396ba14ad89b557fb402b 
-    fi
-    rm -rf "out/error.log"
-    cd ../../../
-  fi
 fi

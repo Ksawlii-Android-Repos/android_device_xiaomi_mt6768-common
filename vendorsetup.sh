293860bd5135b37b0297a7e134ab9c9ea7ac1a6a
Ksawlii <ksawery.blaszczak@proton.me>
2025-03-07 20:31:13 +0100

    mt6768-common: Clone my hardware/xiaomi if dolby doesn't exist
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
+LOS_VER="22.1"
+
 # mediatek codec patch from hipexscape
 if [ ! -f "frameworks/av/.mtk_codec_fix" ]; then
     curl -sL "https://raw.githubusercontent.com/hipexscape/Scripts/tsm/mediatek_codec_patch.sh" | bash
@@ -7,4 +9,11 @@ fi
 # Remove FMRadio
 if [ -d "packages/apps/FMRadio" ]; then
   rm -rf "packages/apps/FMRadio"
+fi
+
+# Dolby
+if [ ! -d "hardware/xiaomi/dolby" ]; then
+  rm -rf "hardware/xiaomi/"
+  echo "Dolby not found, cloning new hardware/xiaomi"
+  git clone https://github.com/Ksawlii-Android-Repos/hardware_xiaomi.git -b "lineage-$LOS_VER" "hardware/xiaomi/"
 fi

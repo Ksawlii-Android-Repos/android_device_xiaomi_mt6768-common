43352b2dae42311fa7b8ab119aadedecaf6740e7
Ksawlii <ksawery.blaszczak@proton.me>
2025-04-08 14:45:35 +0200

    mt6768-common: Remove dolby leftovers
    Signed-off-by: Ksawlii <ksawery.blaszczak@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,3 @@
-LOS_VER="22.1"
-
 # mediatek codec patch from hipexscape
 if [ ! -f "frameworks/av/.mtk_codec_fix" ]; then
     curl -sL "https://raw.githubusercontent.com/hipexscape/Scripts/tsm/mediatek_codec_patch.sh" | bash
@@ -11,10 +9,3 @@ if [ -d "packages/apps/FMRadio" ]; then
   rm -rf "packages/apps/FMRadio"
 fi
 find . -type f -name ".repo/manifests/*.xml" -exec sed -i '/<project path="packages\/apps\/FMRadio" name="LineageOS\/android_packages_apps_FMRadio" \/>/d' {} +
-
-# Dolby
-if [ ! -d "hardware/xiaomi/dolby" ]; then
-  rm -rf "hardware/xiaomi/"
-  echo "Dolby not found, cloning new hardware/xiaomi"
-  git clone https://github.com/Ksawlii-Android-Repos/hardware_xiaomi.git -b "lineage-$LOS_VER" "hardware/xiaomi/"
-fi
